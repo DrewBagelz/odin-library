27263f5d8ec56db36ebba6ec9291b0a840d00efa
DrewBagelz <152040968+DrewBagelz@users.noreply.github.com>
2025-02-11 20:58:35 -0500

    Submit button adds books to myLibrary array

--- script.js
+++ script.js
@@ -14,7 +14,22 @@ function addBookToLibrary(title, author, pages, read) {
 	myLibrary.push(book);
 }
 
+function populateTable() {
+	for (let i = 0; i < myLibrary.length; i++) {}
+}
+
 let newBookBtn = document.querySelector("#new-book");
 newBookBtn.addEventListener("click", () => {
 	form.style.display = "block";
+});
+
+let submitBtn = document.querySelector("#submit");
+submitBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	let title = document.querySelector("#title").value;
+	let author = document.querySelector("#author").value;
+	let pages = document.querySelector("#pages").value;
+	let read = document.querySelector("#read").checked;
+	addBookToLibrary(title, author, pages, read);
+	console.log(myLibrary);
 });
